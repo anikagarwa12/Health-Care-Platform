aaf6ab53c17dc568c0e667889ee9088fbe570225
Anik Agarwal <anikagarwal86@gmail.com>
2024-08-11 02:39:59 +0530

    Your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "javascript-mastery",
-  project: "care-pulse",
+  org: "Anik-Tech",
+  project: "Health-Go",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
